1774f228fc1a3c22eece6f7d66cdfe77c5fb4ef9
Cassio Queiroz de Resende <cass@hl.net.br>
2026-07-14 20:58:13 -0300

    feat: create task model

---
+++ src/models.py
@@ -0,0 +1,6 @@
+from .database import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300))
